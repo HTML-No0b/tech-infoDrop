34b96db34461624b97d09b6110a0f77eea255983
Richard Martin <richie@Richards-MacBook-Air.local>
2022-12-08 14:14:58 -0600

    added more to the routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,15 +1,19 @@
-const router = require('express').Router();
-const {User}= require('../../models')
+const router = require("express").Router();
+const { User } = require("../../models");
 
-router.post('/', async (req,res) => {
-    try{ 
-        const dbPostData = await User.create({
-            username:req.body.username,
-            email:req.body.email,
-            password: req.body.password
-        })
-    } catch(err){
-        console.log(err);
-        res.status(500).json(err)
-    }
+router.post("/", async (req, res) => {
+  try {
+    const dbPostData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      res.status(200).json(dbPostData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
