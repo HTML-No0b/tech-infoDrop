0bd5e842361df7e5673eefb455ba7fb2e20076f9
Html-No0b <rmartin9797@gmail.com>
2023-02-09 17:26:45 -0600

    add handlebars and more to JS

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,20 @@
 async function loginformhandler(event){
     event.preventDefault();
-    const username = document.querySelector('#username-login').value.trim()
-    const password = document.querySelector('#password-login').value.trim()
+    const username = document.querySelector('#username-signup').value.trim()
+    const password = document.querySelector('#password-signup').value.trim()
 
+    if (username && password) {
+        const response = await fetch('/controllers/api/userRoutes.js/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            })
+        })
+        if (response.ok){
+            document.location.replace('/dashboard')
+        }
+        }
 
-}
+    }
+
